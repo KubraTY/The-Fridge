0920b3ccd72ae44994ebc714c21de93f45649d71
mcomptier <labassecourtmusic@gmail.com>
2023-12-22 16:55:59 +0100

    some style on react, commit before new branch

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import classes from "..styles/RecipeCard.module.css"; 
+import classes from '../styles/RecipeCard.module.css' 
 import { useNavigate } from "react-router-dom";
 
 const RecipeCard = ({ id, title, calories, image, servings, remove }) => {
    const navigate = useNavigate()
 
   return (
-    <div className="container" onClick={(event) => {
+    <div className={classes.container} onClick={(event) => {
         navigate(`recipes/${id}`)
     }}>
       <div>
-        <img className="img" src={image} alt={title} />
+        <img className={classes.img} src={image} alt={title} />
       </div>
       <div>
         <h2> {title} </h2>
       </div>
-      <button className="deleteButton" onClick={(event) => {
+      <button className={classes.deleteButton} onClick={(event) => {
         event.stopPropagation
         remove()
     }}>
