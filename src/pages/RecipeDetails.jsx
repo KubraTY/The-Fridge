854ca87faab9d2e68c802f03048abc835129a593
Catherine Fournier <fourniercath@hotmail.com>
2023-12-22 16:18:53 +0100

    add details page with wrong api links

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,9 +1,32 @@
 const RecipeDetails = () => {
-    return(
+
+    const [recipe, setRecipe] = useState()
+    const { recipeId } = useParams()
+
+    const fetchOneRecipe = async () => {
+        try {
+            const response = await fetch(`https://fakestoreapi.com/products/${productId}`)
+            if (response.ok) {
+              const recipeData = await response.json()
+              setRecipe(recipeData)
+            }
+          } catch (error) {
+            console.log(error)
+            navigate('/')
+          }
+    }
+
+    useEffect(() => {
+        fetchOneRecipe()
+      }, [recipeId])
+    
+    return recipe? (
         <>
             <h1>Recipe Name</h1>
         </>
-    )
+    ) : (
+        <h1>Loading..</h1>
+      )
 }
 
 export default RecipeDetails;
