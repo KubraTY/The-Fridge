fe4595d527b684fe4d349c5b2e0f571f85bc5139
mcomptier <labassecourtmusic@gmail.com>
2023-12-22 10:43:08 +0100

    first fetch with fake data

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,9 +1,35 @@
-const SearchResults= () => {
-    return(
-        <>
-            <h1>Search Result</h1>
-        </>
-    )
-}
+import React, { useEffect,useState } from "react";
 
-export default SearchResults
+const SearchResults = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchRecipes = async() => {
+    try { 
+        const response = await fetch('https://fakestoreapi.com/products')
+        console.log(response)
+        if (response.ok){
+         const recipesData = await response.json()
+         console.log(recipesData)
+         setRecipes(recipesData)
+    }
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+useEffect(() => {
+    console.log('useffect ran')
+    fetchRecipes()
+}, []);
+
+  return (
+    <div className="RecipesListPage">
+      <h1>Search Result</h1>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>{recipe.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchResults;
