81988fbb88b909fb1eaa050fb821019c64a3f9d1
mcomptier <labassecourtmusic@gmail.com>
2023-12-22 12:10:50 +0100

    recipeCard

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -2,14 +2,12 @@ import React from "react";
 //import classes from "./RecipeCard.module.css"; 
 import { useNavigate } from "react-router-dom";
 
-
 const RecipeCard = ({ id, title, calories, image, servings, remove }) => {
    const navigate = useNavigate()
 
   return (
     <div className="container" onClick={(event) => {
         navigate(`recipes/${id}`)
-
     }}>
       <div>
         <img className="img" src={image} alt={title} />
