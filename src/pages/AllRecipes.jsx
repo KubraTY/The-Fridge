3cdeed4ae5d8f50ef241d1134c3a531ea2a19cab
mcomptier <labassecourtmusic@gmail.com>
2024-01-08 14:41:34 +0100

    allrecipes

--- src/pages/AllRecipes.jsx
+++ src/pages/AllRecipes.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState,useContext } from "react";
+import React, { useEffect,useContext } from "react";
 import RecipeCard from "../components/RecipeCard";
 import { SimpleGrid } from '@mantine/core';
 import { useViewportSize } from "@mantine/hooks";
