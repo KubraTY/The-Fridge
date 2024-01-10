e0008df2c6714e7ba6c72ceb77b6ec7e2f40a979
mcomptier <labassecourtmusic@gmail.com>
2024-01-10 15:32:44 +0100

    footer static

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import styles from '../styles/Footer.module.css'
-import { AppShell } from '@mantine/core'
+import styles from '../styles/Footer.module.css';
+import { AppShell } from '@mantine/core';
 
 const Footer = () => {
     return (
         <AppShell>
-
-        <AppShell.Footer className={styles.footer}>
-            <div className={styles.footerContent}>
-                <p > 2023 The Fridge. All rights reserved.</p>
-            </div>
-        </AppShell.Footer>
+            <AppShell.Footer className={`${styles.footer} ${styles.customFooter}`}>
+                <div className={styles.footerContent}>
+                    <p>2023 The Fridge. All rights reserved.</p>
+                </div>
+            </AppShell.Footer>
         </AppShell>
     );
 };
-
 
 export default Footer;
